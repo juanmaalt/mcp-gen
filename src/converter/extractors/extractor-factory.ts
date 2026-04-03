1b5f8c2367274b5ec452c1023fd6438bce5b0b8c
Juan M Altamirano <juanmaalt@gmail.com>
2026-04-03 14:23:45 +0200

    add python extractor

--- src/converter/extractors/extractor-factory.ts
+++ src/converter/extractors/extractor-factory.ts
@@ -1,11 +1,14 @@
 import { Extractor, UnsupportedLanguageError } from "@src/converter/extractors/extractor.js";
 import { NodeExtractor } from "@src/converter/extractors/node-extractor.js";
+import { PythonExtractor } from "@src/converter/extractors/python-extractor.js";
 
 export function getExtractor(language: string): Extractor {
     switch (language) {
         case "typescript":
         case "javascript":
             return new NodeExtractor();
+        case "python":
+            return new PythonExtractor();
         default:
             throw new UnsupportedLanguageError(language);
     }
